30f52c9f2e511d1ff5b5f661f6b45f2557535811
trh8614@g.rit.edu <trh8614@g.rit.edu@bc05867c-f7ad-0b5f-35ae-aaeea2e352ed>
2014-03-09 20:11:44 +0000

    Fixed basic view and etc.
    Run using demo server. In working condition.

---  toolshare --username trh8614@g.rit.edu/toolshare/settings.py
+++  toolshare --username trh8614@g.rit.edu/toolshare/settings.py
@@ -11,8 +11,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
+MEDIA_ROOT = PROJECT_PATH + '/media/'
 
+TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/'
+)
+
+STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -80,3 +87,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth"
+)
