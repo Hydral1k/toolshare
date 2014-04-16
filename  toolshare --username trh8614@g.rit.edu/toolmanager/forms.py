d3280f996d28a4dd993563b7eeefc7fbd6c4bd8b
trh8614@g.rit.edu <trh8614@g.rit.edu@bc05867c-f7ad-0b5f-35ae-aaeea2e352ed>
2014-04-16 02:04:16 +0000

    Customized tool forms.py

---
+++  toolshare --username trh8614@g.rit.edu/toolmanager/forms.py
@@ -0,0 +1,43 @@
+"""
+	Toolshare Form
+
+"""
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from toolmanager.models import Tool
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+
+"""
+	A tool form for adding a tool which can be rendered through HTML.
+
+"""
+class ToolForm( forms.Form ):
+
+	fields = ['tool_name', 'tool_manufacture', 'location', 'tool_description', 'quantity', 'quantity_available']
+
+	fields[0] = forms.CharField(max_length=64,
+                                label=_("Tool Name"),
+                                error_messages={'invalid': _("Your input is too long.")})
+
+	fields[1] = forms.CharField(max_length=64,
+                                label=_("Tool Manufacture"),
+                                error_messages={'invalid': _("Your input is too long.")})
+
+	fields[2] = forms.CharField(max_length=32,
+                                label=_("Location"),
+                                error_messages={'invalid': _("Your input is too long.")})
+
+
+    fields[3] = forms.CharField(max_length=128,
+                                label=_("Description"),
+                                error_messages={'invalid': _("Your input is too long.")})
+
+    fields[4] = forms.IntegerField(label=_("Quantity Total")})
+
+    fields[5] = forms.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(int(fields[4]))], 
+                                   error_messages={'invalid': _("Your value is to above the Total Quantity or below 0")},
+                                   label = _("Quantity Available"))
